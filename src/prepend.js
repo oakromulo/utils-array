4f895aa80006d0f645103fa13f85e50c8a905337
Mauro Titimoli <mgtitimoli@gmail.com>
2017-09-19 20:44:42 -0300

    feat(immutable-helpers): Add prepend immutable helper

---
+++ src/prepend.js
@@ -0,0 +1,11 @@
+// @flow
+
+/**
+ * Returns a new Array with elements prepended to the one given
+ */
+const prepend = <Element>(
+  elements: Array<Element>,
+  array: Array<Element>
+): Array<Element> => [...elements, ...array];
+
+export default prepend;
