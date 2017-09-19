0dd647bc4a47c7c73ab300a6b3d8826694c61e98
Mauro Titimoli <mgtitimoli@gmail.com>
2017-09-19 20:44:12 -0300

    feat(immutable-helpers): Add append immutable helper

---
+++ src/append.js
@@ -0,0 +1,11 @@
+// @flow
+
+/**
+ * Returns a new Array with elements appended to the one given
+ */
+const append = <Element>(
+  elements: Array<Element>,
+  array: Array<Element>
+): Array<Element> => [...array, ...elements];
+
+export default append;
