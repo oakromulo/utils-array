57bdb162c68f7ea9d9ed0214b1294fbb57d6be43
Mauro Titimoli <mgtitimoli@gmail.com>
2017-09-21 18:48:13 -0300

    refactor(src): Change args order and update doc

--- src/cycleNext.js
+++ src/cycleNext.js
@@ -3,9 +3,9 @@
 import isLastIndex from "./isLastIndex";
 
 /**
- * Returns 0 if current index is the last one, or returns next if it is not
+ * Returns 0 if current index is the last one, or returns next if it is not.
  */
-const cycleNext = (currentIndex: number, array: Array<*>) =>
-  isLastIndex(currentIndex, array) ? 0 : currentIndex + 1;
+const cycleNext = (array: Array<*>, currentIndex: number) =>
+  isLastIndex(array, currentIndex) ? 0 : currentIndex + 1;
 
 export default cycleNext;
