d371689140b714cb3aa9c8e1b690b5c49fdd3c55
Mauro Titimoli <mgtitimoli@gmail.com>
2017-09-21 18:51:54 -0300

    refactor(src): Index now is autogenerated using create-index dep

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 // @flow
 
-export {default as append} from "./append";
-export {default as convertIfNot} from "./convertIfNot";
-export {default as cycleNext} from "./cycleNext";
-export {default as fromObject} from "./fromObject";
-export {default as insert} from "./insert";
-export {default as isKey} from "./isKey";
-export {default as isLastIndex} from "./isLastIndex";
-export {default as isPossibleFromObject} from "./isPossibleFromObject";
-export {default as prepend} from "./prepend";
-export {default as reduceIf} from "./reduceIf";
-export {default as reduceWhile} from "./reduceWhile";
-export {default as remove} from "./remove";
-export {default as repeat} from "./repeat";
-export {default as replace} from "./replace";
+// @create-index
+
+export {default as append} from "./append.js";
+export {default as convertIfNot} from "./convertIfNot.js";
+export {default as cycleNext} from "./cycleNext.js";
+export {default as fromObject} from "./fromObject.js";
+export {default as insert} from "./insert.js";
+export {default as isKey} from "./isKey.js";
+export {default as isLastIndex} from "./isLastIndex.js";
+export {default as isPossibleFromObject} from "./isPossibleFromObject.js";
+export {default as prepend} from "./prepend.js";
+export {default as reduceIf} from "./reduceIf.js";
+export {default as reduceWhile} from "./reduceWhile.js";
+export {default as remove} from "./remove.js";
+export {default as repeat} from "./repeat.js";
+export {default as replace} from "./replace.js";
+export {default as resolveIndex} from "./resolveIndex.js";
