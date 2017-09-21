420f215339296a8cde3b92509f34605bc1505be3
Mauro Titimoli <mgtitimoli@gmail.com>
2017-09-21 18:50:51 -0300

    refactor(src): Change args order and remove support of rel index

--- src/remove.js
+++ src/remove.js
@@ -1,20 +1,13 @@
 // @flow
 
 /**
- * Returns a new Array with the result of having
- * removed the specified amount (count) of elements at the given index
+ * Returns a new Array with the result of having removed the specified amount
+ * (count) of elements at the given index.
  */
 const remove = <Element>(
-  count: number,
   index: number,
+  count: number,
   array: Array<Element>
-): Array<Element> => {
-  const indexToRemove = index < 0 ? array.length - index : index;
-
-  return [
-    ...array.slice(0, indexToRemove),
-    ...array.slice(indexToRemove + count)
-  ];
-};
+): Array<Element> => [...array.slice(0, index), ...array.slice(index + count)];
 
 export default remove;
